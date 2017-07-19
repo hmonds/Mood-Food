03df43ab0ae278565784a079aa8018b03fc600da
Alexis Mosley-McLemore <amosleym@sci227lab19.spelman.edu>
2017-07-19 13:34:42 -0400

    linked pictures and pages

--- mood-food/main.py
+++ mood-food/main.py
@@ -29,6 +29,10 @@ class MainHandler(webapp2.RequestHandler):
         <body bgcolor = "#fcbfac">
         <center>
         <img src="resources/moodfood.jpg" alt="logo">
+        <a href="/Angry"><img id="angry-face" src="resources/angry.png" alt= "logo"></a>
+        <a href="/Tired"><img src="resources/Sleeping_Emoji.png" alt="Sleepy"></a>
+        <a href="/Sad"><img src="resources/Sad_Face_Emoji.png" alt="Sad"></a>
+        <a href="/Happy"><img src="resources/Happy_Emoji_Icon.png" alt="Happy"></a>
         </center>
 
         '''
@@ -60,20 +64,18 @@ class QuizineHandler(webapp2.RequestHandler):
         <div class="quiz-result"></div>
         </center>
         ''')
+
 class AngryHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('''
         <!DOCTYPE html>
         <head>
-        <title>Happy</title>
+        <title>Angry</title>
         </head>
 
         <body bgcolor = Teal>
         <h1>Angry</h1>
         <ul>
-            <ls>Barley <ul> Description </ul> </ls>
-            <ls>Celery <ul> Description </ul> </ls>
-            <ls>Purple Cabbage <ul> Description </ul> </ls>
             <ls>Apples and Peanut Butter <ul> Boots energy which can help to improve mood </ul> </ls>
             <ls>Walnuts <ul> Rich in Omgea-1 fatty acids which have been proven to make you happier </ul> </ls>
             <ls>Bananas <ul> Rich in potassium and vitamin B. Slows down heart rate and helps to control blood pressure </ul> </ls>
@@ -87,11 +89,75 @@ class AngryHandler(webapp2.RequestHandler):
             <ls>Wasabi <ul> Description </ul> </ls>
             <ls>Ice Cream <ul> It is high in sugar and fats which would release dopamine which causes pleasure </ul> </ls>
             <ls>Baked Potato <ul> Rich in vitamin B which reduces stress levels and reduces blood pressure </ul> </ls>
+            </body>
             </html>
         ''')
+
+class HappyHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('''
+        <!DOCTYPE html>
+        <head>
+        <title>Happy</title>
+        </head>
+
+        <body bgcolor = Teal>
+        <h1>Happy</h1>
+        <ul>
+            <ls> Black Bean Brownies <ul> Rich in iron, fiber, copper, zinc, and potassium  </ul> </ls>
+            <ls> Apricots <ul> Rich in vitamin B6 and antioxidants which are linked to happier moods </ul> </ls>
+            <ls> Halibut <ul> Boost weight-loss and increases serotonin levels </ul> </ls>
+            <ls> Seaweed <ul> Rich in iodine which fights depression </ul> </ls>
+            <ls> Blueberry Juice <ul> Rich in vitamin C which can fight fatigue and depression </ul> </ls>
+            <ls> Chamomile Tea <ul> Helps to readjust the circadian rhythm so that you can get a good night sleep </ul> </ls>
+            </html>
+        ''')
+
+class TiredHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('''
+        <!DOCTYPE html>
+        <head>
+        <title>Tired</title>
+        </head>
+
+        <body bgcolor = Teal>
+        <h1>Tired</h1>
+        <ul>
+            <ls> Salmon <ul> This fish's hefty dose of protein speeds metabolism, which increases energy.</ul> </ls>
+            <ls> Oatmeal <ul> High in fiber and protein and stabilizes blood sugar all day with diabetes, fiber can slow the absorption of sugar and help improve blood sugar levels, lowers the risk of developing type 2 diabetes. </ul> </ls>
+            <ls> Quinoa <ul> Protein and amino acids in this gluten free grain aid in muscle repair and post workout recovery. </ul> </ls>
+            <ls> Blueberries <ul> Potent antixodants combat free radicals like cancer that can injure cells and lead to fatigue. Boost the brain by relieving stress, neurotoxicity, and degeneration. in iodine which fights depression. </ul> </ls>
+        </html>
+    ''')
+
+class SadHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('''
+        <!DOCTYPE html>
+        <head>
+        <title>Sad</title>
+        </head>
+
+        <body bgcolor = Teal>
+        <h1>Sad</h1>
+        <ul>
+            <ls> Cold-Water Fish <ul> Fish is also a great source of lean protein, which stabilizes blood sugar. Eating small amounts of protein with meals can help keep your mood on a more even keel.</ul> </ls>
+            <ls> Ancient Grains <ul> Complex carbohydrates take longer to digest, which means they don't cause spikes in blood sugar that can create roller-coaster moods. Complex carbs also increase levels of serotonin in the brain.</ul> </ls>
+            <ls> Nuts and seeds <ul> Magnesium, a mineral found naturally in nuts and seeds, influences production of serotonin, a "feel-good" brain chemical. Magnesium also affects overall energy production.</ul> </ls>
+            <ls> Baked potato <ul> Potatos are rich in Vitamin B complex, reduce stress and lower blood pressure. </ul> </ls>
+            <ls> Apple and Peanut Butter <ul> A great combo of carbs and protein: satisfying, energy-boosting and nourishing for the brain. The perfect comfort food!</ul> </ls>
+            </body>
+            </html>
+        ''')
+
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/Quizine', QuizineHandler),
+    ('/Angry', AngryHandler),
+    ('/Happy', HappyHandler),
+    ('/Sad', SadHandler),
+    ('/Tired', TiredHandler)
 ], debug=True)
